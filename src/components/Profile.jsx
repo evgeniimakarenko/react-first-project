643b5254cf1089c68d22e620e1a6fac934cb4678
Evgenii Makarenko <evgeniimakarenko532@gmail.com>
2023-06-07 16:04:58 +0300

    add posts

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -15,6 +15,11 @@ const Profile = () => {
         <div>New post</div>
         <div>post 1</div>
         <div>post 2</div>
+        <div>post 3</div>
+        <div>post 4</div>
+        <div>post 5</div>
+        <div>post 6</div>
+        <div>post 7</div>
       </div>
     </div>
   );
