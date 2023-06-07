4476a28e6683f4834fe993676d5af3e12cd2972d
Evgenii Makarenko <evgeniimakarenko532@gmail.com>
2023-06-07 15:57:37 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Electron_Software_Framework_Logo.svg/256px-Electron_Software_Framework_Logo.svg.png" />
+    </header>
+  );
+};
+
+export default Header;
